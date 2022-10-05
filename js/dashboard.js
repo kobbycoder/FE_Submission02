c0fd2f3dcfa574eb753fef81088746f0d98245c7
alexanderosman <alexanderosman11@gmail.com>
2022-10-05 19:30:01 +0000

    dashboard data summary completed

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,8 @@
 document.documentElement.classList.remove('no-js')
+const today = document.getElementById("today");
+const lastWeek = document.getElementById("lastweek");
+const lastMonth = document.getElementById("lastmonth");
+
 document.getElementById('logOut').onclick = function logout() {
     localStorage.removeItem("token");
     window.location.href = './index.html'
@@ -23,7 +27,31 @@ fetch(url, {
     if (json.msg) {
         checkAndRquest()
     } else {
-        console.log(json);
+        const weekData = json.dashboard.sales_over_time_week
+        const monthsData = json.dashboard.sales_over_time_year
+        console.log(weekData);
+        var todayTotal, todayOrders;
+        var lastWeekTotal = 0;
+        var lastWeekOrders = 0;
+        var lastMonthTotal = 0;
+        var lastMonthOrders = 0;
+        for ( var i in weekData) {
+            todayTotal = weekData[i].total
+            todayOrders = weekData[i].orders
+            break;
+        }
+        today.innerHTML = `$${nFormatter(todayTotal)} / ${todayOrders} orders`
+        
+        for (var k = 1; k < Object.keys(weekData).length + 1; k++){
+            lastWeekTotal += weekData[k].total
+            lastWeekOrders += weekData[k].orders
+        }
+        lastWeek.innerHTML = `$${nFormatter(lastWeekTotal)} / ${lastWeekOrders} orders`
+
+        var lastmonth = Object.keys(monthsData)[1]
+        lastMonthTotal = monthsData[lastmonth].total
+        lastMonthOrders = monthsData[lastmonth].orders
+        lastMonth.innerHTML = `$${nFormatter(lastMonthTotal)} / ${lastMonthOrders} orders`
     }
 })
 .catch(err => console.log(err));
@@ -46,22 +74,17 @@ fetch(refreshUrl, {
     .catch(err => console.log(err));
 }
 
-var weekCanvasElement = document.getElementById("weekChart");
-var config = {
-    type: "bar",
-    data: {
-        labels: ["day","day","day","day"], 
-        datasets: [
-            {
-                label: "Number of cookies", 
-                data: [5, 2, 12, 19, 3],
-                backgroundColor: "#D8D8D8",
-                borderColor: "#C3C3C3",
-            }
-        ],
 
+function nFormatter(num) {
+    if (num >= 1000000000) {
+        return(num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
     }
+    if (num >= 1000000) {
+        return(num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+    }
+    if (num >= 1000) {
+        return(num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    }
+    return num;
 }
-
-var weekChart = new Chart(weekCanvasElement, config)
 
