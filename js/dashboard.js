aa8950bab72461a946d7deef69c7476b0bb8bf53
alexanderosman <alexanderosman11@gmail.com>
2022-10-06 01:48:29 +0000

    dashboard charts and toggle completed

--- js/dashboard.js
+++ js/dashboard.js
@@ -30,6 +30,10 @@ fetch(url, {
         const weekData = json.dashboard.sales_over_time_week
         const monthsData = json.dashboard.sales_over_time_year
         console.log(weekData);
+        console.log(monthsData);
+        barWeekChart(weekData)
+        barMonthChart(monthsData)
+        
         var todayTotal, todayOrders;
         var lastWeekTotal = 0;
         var lastWeekOrders = 0;
@@ -86,5 +90,92 @@ function nFormatter(num) {
         return(num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
     }
     return num;
+}
+
+const barWeekChart = (data) => {
+    const revenueList = []
+    var allData = []
+    const day = ["today", "yesterday", "day 3","day 4", "day 5", "day 6", "day 7"]
+    var sum = 0
+    var size = Object.keys(data).length;
+
+    for (var i = 1; i < size +1; i++) {
+        console.log(i);
+        revenueList.push({day:day[i-1],value:data[i].total})
+        allData = [...revenueList]
+        sum +=data[i].total
+    }
+    console.log(allData);
+    let myWeek = allData.map((item) => ({
+        day:item.day,
+        percent: (item.value/sum*100)+1
+    }))
+
+    const chart = document.querySelector(".week-stats");
+
+    const createBarItems = (rev) => {
+        let item = document.createElement("div")
+        let bar = document.createElement("span")
+        let title = document.createElement("span")
+
+        bar.style = `height: ${rev.percent+50}%`;
+        title.innerHTML = rev.day
+
+        bar.append(title)
+        item.append(bar)
+
+        return item;
+    }
+
+    myWeek.forEach((rev) => chart.append(createBarItems(rev)))
+}
+
+const ShowHideDiv = (checkChart) => {
+    var week = document.getElementById("myWeek")
+    var month = document.getElementById("myMonth")
+    var title = document.getElementById("statsTitle")
+
+    title.innerHTML = checkChart.checked ? "Revenue (last 12 months)" : "Revenue (last 7 days)"
+    week.style.display = checkChart.checked ? "none" : "flex"
+    month.style.display = checkChart.checked ? "flex" : "none"
+}
+
+
+const barMonthChart = (data) => {
+     const revenueList = []
+    var allData = []
+    const day = ["this month", "last month", "month 3","month 4", "month 5", "month 6", "month 7", "month 8", "month 9", "month 10", "month 11", "month 12"]
+    var sum = 0
+    var size = Object.keys(data).length;
+
+    for (var i = 1; i < size +1; i++) {
+        console.log(i);
+        revenueList.push({day:day[i-1],value:data[i].total})
+        allData = [...revenueList]
+        sum +=data[i].total
+    }
+    console.log(allData);
+    let myWeek = allData.map((item) => ({
+        day:item.day,
+        percent: (item.value/sum*100)
+    }))
+
+    const chart = document.querySelector(".month-stats");
+
+    const createBarItems = (rev) => {
+        let item = document.createElement("div")
+        let bar = document.createElement("span")
+        let title = document.createElement("span")
+
+        bar.style = `height: ${rev.percent+50}%`;
+        title.innerHTML = rev.day
+
+        bar.append(title)
+        item.append(bar)
+
+        return item;
+    }
+
+    myWeek.forEach((rev) => chart.append(createBarItems(rev)))
 }
 
